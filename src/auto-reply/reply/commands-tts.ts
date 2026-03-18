dadasersin/openclaw-yapay-zeka-asistan-

TTS: add Typecast provider (emotion presets, audio tuning, Asian language voices)
Adds Typecast by Neosapience as a TTS provider.

- TtsProvider type + Zod schema with full config validation
- typecastTTS() in standard + telephony flows (WAV sample rate parsed from header)
- TYPECAST_API_KEY env var; fallback order: openai → elevenlabs → typecast → edge
- Gateway RPC + CLI + voice-call extension
- Docs (en); zh-CN will follow after i18n pipeline
- Fix: getTtsProvider() now falls back to typecast when only TYPECAST_API_KEY is set

Rebased onto 2026.3.8 (upstream/main).

src/auto-reply/reply/commands-tts.ts
@@ -56,7 +56,8 @@ function ttsUsage(): ReplyPayload {
       `**Providers:**\n` +
       `• edge — Free, fast (default)\n` +
       `• openai — High quality (requires API key)\n` +
-      `• elevenlabs — Premium voices (requires API key)\n\n` +
+      `• elevenlabs — Premium voices (requires API key)\n` +
+      `• typecast — AI voices with emotion control (requires API key)\n\n` +
       `**Text Limit (default: 1500, max: 4096):**\n` +
       `When text exceeds the limit:\n` +
       `• Summary ON: AI summarizes, then generates audio\n` +
@@ -161,6 +162,7 @@ export const handleTtsCommands: CommandHandler = async (params, allowTextCommand
     if (!args.trim()) {
       const hasOpenAI = Boolean(resolveTtsApiKey(config, "openai"));
       const hasElevenLabs = Boolean(resolveTtsApiKey(config, "elevenlabs"));
+      const hasTypecast = Boolean(resolveTtsApiKey(config, "typecast"));
       const hasEdge = isTtsProviderConfigured(config, "edge");
       return {
         shouldContinue: false,
@@ -170,14 +172,20 @@ export const handleTtsCommands: CommandHandler = async (params, allowTextCommand
             `Primary: ${currentProvider}\n` +
             `OpenAI key: ${hasOpenAI ? "✅" : "❌"}\n` +
             `ElevenLabs key: ${hasElevenLabs ? "✅" : "❌"}\n` +
+            `Typecast key: ${hasTypecast ? "✅" : "❌"}\n` +
             `Edge enabled: ${hasEdge ? "✅" : "❌"}\n` +
-            `Usage: /tts provider openai | elevenlabs | edge`,
+            `Usage: /tts provider openai | elevenlabs | typecast | edge`,
         },
       };
     }
 
     const requested = args.trim().toLowerCase();
-    if (requested !== "openai" && requested !== "elevenlabs" && requested !== "edge") {
+    if (
+      requested !== "openai" &&
+      requested !== "elevenlabs" &&
+      requested !== "typecast" &&
+      requested !== "edge"
+    ) {
       return { shouldContinue: false, reply: ttsUsage() };
     }
 

hungjcc/openclaw

src/auto-reply/reply/commands-tts.ts
@@ -57,7 +57,8 @@ function ttsUsage(): ReplyPayload {
       `**Providers:**\n` +
       `• microsoft — Microsoft Edge-backed speech (default fallback)\n` +
       `• openai — High quality (requires API key)\n` +
-      `• elevenlabs — Premium voices (requires API key)\n\n` +
+      `• elevenlabs — Premium voices (requires API key)\n` +
+      `• typecast — AI voices with emotion control (requires API key)\n\n` +
       `**Text Limit (default: 1500, max: 4096):**\n` +
       `When text exceeds the limit:\n` +
       `• Summary ON: AI summarizes, then generates audio\n` +
@@ -162,6 +163,7 @@ export const handleTtsCommands: CommandHandler = async (params, allowTextCommand
     if (!args.trim()) {
       const hasOpenAI = Boolean(resolveTtsApiKey(config, "openai"));
       const hasElevenLabs = Boolean(resolveTtsApiKey(config, "elevenlabs"));
+      const hasTypecast = Boolean(resolveTtsApiKey(config, "typecast"));
       const hasMicrosoft = isTtsProviderConfigured(config, "microsoft", params.cfg);
       return {
         shouldContinue: false,
@@ -171,8 +173,9 @@ export const handleTtsCommands: CommandHandler = async (params, allowTextCommand
             `Primary: ${currentProvider}\n` +
             `OpenAI key: ${hasOpenAI ? "✅" : "❌"}\n` +
             `ElevenLabs key: ${hasElevenLabs ? "✅" : "❌"}\n` +
+            `Typecast key: ${hasTypecast ? "✅" : "❌"}\n` +
             `Microsoft enabled: ${hasMicrosoft ? "✅" : "❌"}\n` +
-            `Usage: /tts provider openai | elevenlabs | microsoft`,
+            `Usage: /tts provider openai | elevenlabs | microsoft | typecast`,
         },
       };
     }